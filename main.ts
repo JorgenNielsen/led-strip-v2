5e368ecd207561317c9a34821bf6a74ab6c2b96a
JorgenNielsen <50008532+JorgenNielsen@users.noreply.github.com>
2024-02-05 20:17:59 +0100

    Dyn strip len

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    for (let indeks = 0; indeks <= 60; indeks++) {
+    for (let indeks = 0; indeks <= top; indeks++) {
         counter = indeks
         strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Red))
         strip.show()
@@ -32,7 +32,7 @@ input.onButtonPressed(Button.B, function () {
         valA = liste[index]
         valB = liste1[index]
         serial.writeValue("x", index)
-        for (let indeks2 = 0; indeks2 <= 59; indeks2++) {
+        for (let indeks2 = 0; indeks2 <= top; indeks2++) {
             strip.setPixelColor(indeks2, valA)
             strip.show()
             strip.setPixelColor(indeks2 + 1, valB)
@@ -41,15 +41,16 @@ input.onButtonPressed(Button.B, function () {
             strip.clear()
         }
         strip.clear()
-        strip.setPixelColor(59, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(top - 1, neopixel.colors(NeoPixelColors.Red))
         strip.show()
     }
-    for (let indeks3 = 0; indeks3 <= 60; indeks3++) {
-        counter = 59 - indeks3
+    for (let indeks3 = 0; indeks3 <= top; indeks3++) {
+        counter = top - indeks3
         strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Red))
         strip.show()
         basic.pause(2)
-        strip.clear()
+        strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Black))
+        strip.show()
     }
 })
 let valB = 0
@@ -58,5 +59,8 @@ let liste: number[] = []
 let liste1: number[] = []
 let counter = 0
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P1, 60, NeoPixelMode.RGB)
+let top = 0
+top = 180
+music.play(music.createSoundExpression(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+strip = neopixel.create(DigitalPin.P1, top, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
