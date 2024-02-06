3f1343f8fc3b52a865e8c24b5cc553cbcff9f565
JorgenNielsen <50008532+JorgenNielsen@users.noreply.github.com>
2024-02-06 20:32:32 +0100

    light ajust

--- main.ts
+++ main.ts
@@ -37,29 +37,31 @@ input.onButtonPressed(Button.B, function () {
     neopixel.colors(NeoPixelColors.Indigo),
     neopixel.colors(NeoPixelColors.White)
     ]
-    for (let index = 0; index <= liste1.length - 1; index++) {
-        valA = liste[index]
-        valB = liste1[index]
-        serial.writeValue("x", index)
-        for (let indeks22 = 0; indeks22 <= top; indeks22++) {
-            strip.setPixelColor(indeks22, valA)
-            strip.show()
-            strip.setPixelColor(indeks22 + 1, valB)
-            strip.show()
-            basic.pause(5)
+    for (let indeks = 0; indeks <= 3; indeks++) {
+        for (let index = 0; index <= liste1.length - 1; index++) {
+            valA = liste[index]
+            valB = liste1[index]
+            serial.writeValue("x", index)
+            for (let indeks22 = 0; indeks22 <= top; indeks22++) {
+                strip.setPixelColor(indeks22, valA)
+                strip.show()
+                strip.setPixelColor(indeks22 + 1, valB)
+                strip.show()
+                basic.pause(2)
+                strip.clear()
+            }
             strip.clear()
+            strip.setPixelColor(top - 1, neopixel.colors(NeoPixelColors.Red))
+            strip.show()
         }
-        strip.clear()
-        strip.setPixelColor(top - 1, neopixel.colors(NeoPixelColors.Red))
-        strip.show()
-    }
-    for (let indeks3 = 0; indeks3 <= top; indeks3++) {
-        counter = top - indeks3
-        strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Red))
-        strip.show()
-        basic.pause(2)
-        strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Black))
-        strip.show()
+        for (let indeks3 = 0; indeks3 <= top; indeks3++) {
+            counter = top - indeks3
+            strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Red))
+            strip.show()
+            basic.pause(1)
+            strip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Black))
+            strip.show()
+        }
     }
 })
 let valB = 0
@@ -71,4 +73,7 @@ let strip: neopixel.Strip = null
 let top = 0
 top = 60
 strip = neopixel.create(DigitalPin.P1, top, NeoPixelMode.RGB)
+strip.showColor(neopixel.hsl(50, 120, 70))
+strip.show()
+basic.pause(5000)
 strip.showRainbow(1, 120)
